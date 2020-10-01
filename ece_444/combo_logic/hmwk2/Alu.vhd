8219053f9b0ea052d5b7119713d150b51e4883ca
apple <evankrimp@gmail.com>
2020-09-30 23:03:48 -0400

    Alu draft finished ece444

--- ece_444/combo_logic/hmwk2/Alu.vhd
+++ ece_444/combo_logic/hmwk2/Alu.vhd
@@ -10,7 +10,7 @@ entity Alu is
     port
     (
         Result      :   out std_logic_vector(bit_depth-1 downto 0);
-        Error       :   out std_logic;
+        Error       :   out std_logic := 0;
         A           :   in std_logic_vector(bit_depth-1 downto 0);
         B           :   in std_logic_vector(bit_depth-1 downto 0);
         OpCode      :   in std_logic_vector(3 downto 0)
@@ -20,7 +20,6 @@ end Alu;
 architecture behavior of Alu is
 signal A_int        :   integer := 0;
 signal B_int        :   integer := 0;
-signal Result_int   :   integer := 0;
 
 A_int = to_integer(signed(A));
 B_int = to_integer(signed(B));
@@ -29,39 +28,40 @@ begin
     begin
         case OpCode is
             when "0000" =>
-                Result_int <= not A_int;
+                Result <= not A;
             when "0001" =>
-                Result_int <= not B_int;
+                Result <= not B;
             when "0010" =>
-                Result_int <= A_int + B_int;
+                Result <= std_logic_vector(A_int + B_int);
             when "0011" =>
-                Result_int <= A_int - B_int;
+                Result <= std_logic_vector(A_int - B_int);
             when "0100" =>
-                Result_int <= A_int * 2;
+                Result <= std_logic_vector(A_int sll 1);
             when "0101" =>
-                Result_int <= A_int / 4;
+                Result <= std_logic_vector(A_int srl 2);
             when "0110" =>
-                Result_int <= B_int * 2;
+                Result <= std_logic_vector(B_int sll 1);
             when "0111" =>
-                Result_int <= B_int / 4;
+                Result <= std_logic_vector(B_int srl 2);
             when "1000" =>
                 Flip:
                 for i in 0 to bit_depth-1 generate
-                    A_int(i) <= A_int(bit_depth-1-i);
+                    A(i) <= A(bit_depth-1-i);
                 end generate Flip;
+                result <= A;
             when "1001" =>
                 Flip:
                 for i in 0 to bit_depth-1 generate
-                    B_int(i) <= B_int(bit_depth-1-i);
+                    B(i) <= B(bit_depth-1-i);
                 end generate Flip;
+                result <= B;
             when "1010" =>
-                Result_int <= A_int and B_int;
+                Result <= A and B;
             when "1011" =>
-                Result_int <= A_int or B_int;
-            when "1100" | "1101" | "1110" | "1111" =>
-                Error <= "1";
+                Result <= A or B;
             when others =>
-                Result <= (others => "X");
+                Error <= '1';
+                Result <= (others => 'X');
         end case;
     end process op_process;
 end behavior;
