0425ac5279c1c1b6b96ed88f6b493b11318044e1
apple <evankrimp@gmail.com>
2020-11-23 12:37:45 -0500

    ece444 finished first draft of the code

---
+++ ece_444/sequential_logic/hmwk4/SMult.tcl
@@ -0,0 +1,22 @@
+transcript off
+# stop previous simulations
+quit -sim
+
+# select a directory for creation of the work directory
+cd {C:\Users\User\Documents\College\Senior Year\autumn-2020\ece_444\sequential_logic\hmwk4}
+vlib work
+vmap work work
+
+# compile the program and test-bench files
+vcom sim_mem_init.vhd
+vcom SMult.vhd
+vcom test_SMult.vhd
+
+# initialize the simulation window and add waves to the simulation window
+vsim test_SMult
+add wave sim:/test_SMult/dev_to_test/*
+
+# define simulation time
+run 530 ns
+# zoom out
+wave zoom full
