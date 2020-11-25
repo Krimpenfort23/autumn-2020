31f12584086cf4c0ecda944b4aa966b176fb9cc2
Krimpenfort23 <krimpenforte1@udayton.edu>
2020-11-24 22:29:13 -0500

    cps499 Lab9 complete

--- assignments/assignment2/www.myblog.com/classes/db.php
+++ assignments/assignment2/www.myblog.com/classes/db.php
@@ -1,6 +1,6 @@
 <?php
 
-    $dblink = mysqli_connect("localhost", "<database-username>", "<database-password>","blog");
+    $dblink = mysqli_connect("localhost", "krimpenforte1", "bobgeorge","blog");
     if (mysqli_connect_errno()) {
     	printf("Connect failed: %s\n", mysqli_connect_error());
     	exit();
