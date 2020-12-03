8c6ffd9453bef10345462ae33f1b3d698352fc63
Krimpenfort23 <krimpenforte1@udayton.edu>
2020-12-03 03:56:46 -0500

    Task II completed cps499

--- assignments/assignment2/www.myblog.com/classes/auth.php
+++ assignments/assignment2/www.myblog.com/classes/auth.php
@@ -1,4 +1,9 @@
 <?php
+  $httponly = TRUE;
+  $path = "/";
+  $domain = "krimpenforte1.myblog.com"; // Your local domain
+  $secure = TRUE;
+  session_set_cookie_params (60*15,$path,$domain,$secure,$httponly);
   session_start();
   require('../classes/db.php'); 
   require('../classes/user.php'); 
@@ -7,12 +12,16 @@
     if (User::login($_POST["user"],$_POST["password"])){
       $_SESSION["admin"] = User::SITE;
       $_SESSION["user"] = $_POST["user"];
+      $_SESSION["browser"] = $_SERVER["HTTP_USER_AGENT"];
     }  
       
-
   if (!isset($_SESSION["admin"] ) or $_SESSION["admin"] != User::SITE) {
     header( 'Location: /admin/login.php' ) ;
     die();
   }
-  
+  if ($_SESSION["browser"] != $_SERVER["HTTP_USER_AGENT"])
+  {
+    echo "<script>alert('Session Hijacking Detected!')</script>";
+    die();
+  }
 ?>
